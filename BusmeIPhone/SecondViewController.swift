2f7fc9c5f4102e5ab05190e94808ad8917ebba38
Polar Humenn <polar@adiron.com>
2014-12-30 17:07:47 -0500

    Initial Commit

---
+++ BusmeIPhone/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BusmeIPhone
+//
+//  Created by Polar Humenn on 12/30/14.
+//  Copyright (c) 2014 Polar Humenn. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
